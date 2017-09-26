16c4e1fbddb6dea95e6b507c43f05641a66beaae
Vladislav Cheparin <ivladislav969@gmail.com>
2017-09-26 16:37:02 +0300

    fast pollard

--- src/main/java/algorithm/Pollard.java
+++ src/main/java/algorithm/Pollard.java
@@ -1,11 +1,36 @@
 package algorithm;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 /**
  * Created by ASUS-PC on 25.09.2017.
  */
 public class Pollard {
+
+    public static BigInteger f(BigInteger x, BigInteger c, BigInteger N) {
+        return x.multiply(x).mod(N).add(c);
+    }
+
+    public BigInteger divisor(BigInteger N) {
+        final BigInteger TWO = BigInteger.valueOf(2);
+        BigInteger c = new BigInteger(N.bitLength(), new Random());
+        BigInteger x = new BigInteger(N.bitLength(), new Random());
+        BigInteger y = new BigInteger(x.toString());
+        BigInteger divisor = BigInteger.ONE;
+
+        if (N.mod(TWO).compareTo(BigInteger.ZERO) == 0) {
+            return TWO;
+        }
+
+        while (divisor.compareTo(BigInteger.ONE) == 0) {
+            x = f(x, c, N);
+            y = f(f(y, c, N), c, N);
+            divisor = x.subtract(y).gcd(N);
+        }
+        return divisor;
+
+    }
 
     public BigInteger pollard(BigInteger N) {
 //        BigInteger x = new BigInteger(N.bitLength(), new Random());
@@ -26,12 +51,12 @@ public class Pollard {
             if (i == stage) {
                 y = x;
                 stage *= 2;
-                System.out.println(
-                        "x = " + x + "  " +
-                                " y = " + y +
-                                " nod = " + nod +
-                                " i = " + i
-                );
+//                System.out.println(
+//                        "x = " + x + "  " +
+//                                " y = " + y +
+//                                " nod = " + nod +
+//                                " i = " + i
+//                );
             }
             x = x.pow(2)
                     .subtract(BigInteger.ONE)
@@ -49,11 +74,44 @@ public class Pollard {
     }
 
     public static void main(String[] args) {
-        BigInteger pollard = new Pollard()
-                .pollard(new BigInteger(
-                       "344572667627327576872986520507"
-                      //  "1387"
-                ));
-        System.out.println(pollard);
+////        BigInteger pollard = new Pollard()
+////                .pollard(new BigInteger(
+////                       "344572667627327576872986520507"
+////                      //  "1387"
+////                ));
+//        System.out.println(pollard);
+
+//        final PrimeGenerator generator = new PrimeGenerator();
+//        final BigInteger first = generator.primeGenerationBySize(12);
+//        final BigInteger second = generator.primeGenerationBySize(12);
+//        System.out.println(first + " " + second);
+
+        final long start1 = System.currentTimeMillis();
+        final BigInteger N = new BigInteger("5087308846537040504749735052677");
+        final BigInteger e = new BigInteger("635964111226216674951775514511");
+        final BigInteger SW = new BigInteger("3009767917863379873938622968631");
+        final BigInteger divisorP = new Pollard().divisor(N);
+        final BigInteger divisorQ = N.divide(divisorP);
+        final BigInteger fN = divisorP.subtract(BigInteger.ONE)
+                .multiply(
+                        divisorQ.subtract(BigInteger.ONE)
+                );
+        final Euclid euclid = new Euclid().countEuclid(fN, e);
+        final BigInteger d = new BigInteger(
+                euclid.getY()
+        );
+        final BigInteger bigInteger = new PowMod().powMod(SW, d, N);
+        System.out.println(bigInteger);
+
+        final long end1 = System.currentTimeMillis();
+        System.out.println((end1 - start1) / 1000);
+
+
+
+//        final long start = System.currentTimeMillis();
+//        System.out.println(new Pollard().pollard(first.multiply(second)));
+//        final long end = System.currentTimeMillis();
+//        System.out.println((end - start) / 1000);
+
     }
 }
