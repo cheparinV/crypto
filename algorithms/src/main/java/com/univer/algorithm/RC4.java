4e69d2e7faaef814df21b4bb5d3cb5e2c5f56481
Vladislav Cheparin <vladislav.cheparin.gdc@ts.fujitsu.com>
2017-12-28 19:49:11 +0300

    rc4

---
+++ algorithms/src/main/java/com/univer/algorithm/RC4.java
@@ -0,0 +1,145 @@
+package com.univer.algorithm;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * @author Vladislav Cheparin (vladislav.cheparin.gdc@ts.fujitsu.com)
+ */
+public class RC4 {
+    private static final int BYTE_LENGTH = 256;
+    private static final int KEY_MIN_LENGTH = 5;
+    /**
+     * Key array
+     */
+    private byte[] key = new byte[BYTE_LENGTH - 1];
+    /**
+     * Sbox
+     */
+    private int[] sbox = new int[BYTE_LENGTH];
+
+    public RC4() {
+        reset();
+    }
+
+    public RC4(String key) throws InvalidKeyException {
+        this();
+        setKey(key);
+    }
+
+    private void reset() {
+        Arrays.fill(key, (byte) 0);
+        Arrays.fill(sbox, 0);
+    }
+
+    /**
+     * Encrypt given message String with given Charset and key
+     *
+     * @param message message to be encrypted
+     * @param charset charset of message
+     * @param key     key
+     * @return encrypted message
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public byte[] encryptMessage(String message, String key) {
+        reset();
+        setKey(key);
+        byte[] crypt = crypt(message.getBytes());
+        reset();
+        return crypt;
+    }
+
+    /**
+     * Decrypt given byte[] message array with given charset and key
+     *
+     * @param message message to be decrypted
+     * @param charset charset of message
+     * @param key     key
+     * @return string in given charset
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public String decryptMessage(byte[] message, String key)
+            throws InvalidKeyException {
+        reset();
+        setKey(key);
+        byte[] msg = crypt(message);
+        reset();
+        return new String(msg);
+    }
+
+    /**
+     * Crypt given byte array. Be aware, that you must init key, before using
+     * crypt.
+     *
+     * @param msg array to be crypt
+     * @return crypted byte array
+     * @see <a href="http://en.wikipedia.org/wiki/RC4#Pseudo-random_generation_algorithm_.28PRGA.29">Pseudo-random generation algorithm</a>
+     */
+    public byte[] crypt(final byte[] msg) {
+        sbox = initSBox(key);
+        byte[] code = new byte[msg.length];
+        int i = 0;
+        int j = 0;
+        for (int n = 0; n < msg.length; n++) {
+            i = (i + 1) % BYTE_LENGTH;
+            j = (j + sbox[i]) % BYTE_LENGTH;
+            swap(i, j, sbox);
+            int rand = sbox[(sbox[i] + sbox[j]) % BYTE_LENGTH];
+            code[n] = (byte) (rand ^ msg[n]);
+        }
+        return code;
+    }
+
+    private int[] initSBox(byte[] key) {
+        int[] sbox = new int[BYTE_LENGTH];
+        int j = 0;
+
+        for (int i = 0; i < BYTE_LENGTH; i++) {
+            sbox[i] = i;
+        }
+
+        for (int i = 0; i < BYTE_LENGTH; i++) {
+            j = (j + sbox[i] + (key[i % key.length]) & 0xFF) % BYTE_LENGTH;
+            swap(i, j, sbox);
+        }
+        return sbox;
+    }
+
+    private void swap(int i, int j, int[] sbox) {
+        int temp = sbox[i];
+        sbox[i] = sbox[j];
+        sbox[j] = temp;
+    }
+
+    /**
+     * Setup key
+     *
+     * @param key key to be setup
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public void setKey(String key) {
+        if (!(key.length() >= KEY_MIN_LENGTH && key.length() < BYTE_LENGTH)) {
+            throw new InvalidKeyException("Key length has to be between "
+                    + KEY_MIN_LENGTH + " and " + (BYTE_LENGTH - 1));
+        }
+
+        this.key = key.getBytes();
+    }
+
+}
+
+/**
+ * Exception made for recognise invalid keys
+ *
+ * @author Iurii Sergiichuk
+ */
+class InvalidKeyException extends RuntimeException {
+
+    private static final long serialVersionUID = -2412232436238451574L;
+
+    public InvalidKeyException(String message) {
+        super(message);
+    }
+
+}
---
+++ algorithms/src/test/java/com/univer/algorithm/RC4Test.java
@@ -0,0 +1,46 @@
+package com.univer.algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * @author Vladislav Cheparin (vladislav.cheparin.gdc@ts.fujitsu.com)
+ */
+public class RC4Test {
+
+    @Test
+    public void testCryptMessage() {
+        RC4 rc4 = new RC4();
+        String message = "Hello, World!";
+        String key = "This is pretty long key";
+        byte[] crypt = rc4.encryptMessage(message, key);
+        String msg = rc4.decryptMessage(crypt, key);
+        assertEquals(message, msg);
+    }
+
+    @Test
+    public void testCryptWithNonEnglishCharacters() {
+        String message = "Привет, Мир!";
+        String key = "Это довольно длинный ключ";
+        RC4 rc4 = new RC4(key);
+        byte[] crypt = rc4.crypt(message.getBytes());
+        byte[] msg = rc4.crypt(crypt);
+        assertEquals(message, new String(msg));
+    }
+
+    @Test(expected = InvalidKeyException.class)
+    public void testSetKeyShouldThrowInvalidKeyExceptionWithTooSmallKey() {
+        new RC4("");
+    }
+
+    @Test(expected = InvalidKeyException.class)
+    public void testSetKeyShouldThrowInvalidKeyExceptionWithTooBigKey() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 512; i++) {
+            sb.append('a');
+        }
+        RC4 rc4 = new RC4();
+        rc4.setKey(sb.toString());
+    }
+
+}
